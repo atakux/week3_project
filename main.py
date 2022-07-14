c9d58941420159e745b71dcd91a0f35098b03475
atakux <rockyangela5@gmail.com>
2022-07-14 10:40:10 -0700

    added time api

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
+import requests
 app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
+
+
+def time_api():
+    url = "http://worldtimeapi.org/api/PST8PDT"
+
+    response = requests.get(url)
+
+    for key, val in response.json().items():
+        if key == 'datetime':
+            date = val.split('T')[0]
+            time_long = val.split('T')[1]
+            time = time_long.split('.')[0]
+            print(date)
+            print(time)
+            return [date, time]
 
 
 @app.route("/")                          # this tells you the URL the method below is related to
 def home_page():
-    return render_template("home.html")        # this prints HTML to the webpage
+    date = time_api()
+    return render_template("home.html", var=date[0])        # this prints HTML to the webpage
 
 
 @app.route("/page_2")
